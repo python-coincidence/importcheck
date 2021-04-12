78e699c7eaed71e6f031a086cf1289e7c3750733
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-04-12 21:33:47 +0100

    Bump version v0.3.0 -> v0.4.0

--- importcheck/__init__.py
+++ importcheck/__init__.py
@@ -63,7 +63,7 @@ __all__ = [
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.3.0"
+__version__: str = "0.4.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 _module = Plural("module", "modules")
