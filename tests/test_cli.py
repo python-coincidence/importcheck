a903dd9fd7ea340f1dd97d25551aa78413cb14ae
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-05-09 11:34:56 +0100

    Test on newer Python and click versions

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -55,15 +55,19 @@ not_pp = not_pypy(reason="Output differs on PyPy")
 				pytest.param("3.6", marks=[only_version(3.6, reason="Output differs on Python 3.6"), not_pp]),
 				pytest.param("3.7", marks=[only_version(3.7, reason="Output differs on Python 3.7"), not_pp]),
 				pytest.param(
-						"3.6-pypy", marks=[only_version(3.6, reason="Output differs on Python 3.6"), only_pp]
+						"3.6-pypy",
+						marks=[only_version(3.6, reason="Output differs on Python 3.6"), only_pp],
 						),
 				pytest.param(
-						"3.7-pypy", marks=[only_version(3.7, reason="Output differs on Python 3.7"), only_pp]
+						"3.7-pypy",
+						marks=[only_version(3.7, reason="Output differs on Python 3.7"), only_pp],
 						),
 				pytest.param("3.8", marks=only_version(3.8, reason="Output differs on Python 3.8")),
 				pytest.param("3.9", marks=only_version(3.9, reason="Output differs on Python 3.9")),
 				pytest.param("3.10", marks=only_version("3.10", reason="Output differs on Python 3.10")),
-				])
+				pytest.param("3.11", marks=only_version("3.11", reason="Output differs on Python 3.11")),
+				]
+		)
 def version(request):
 	return request.param
 
